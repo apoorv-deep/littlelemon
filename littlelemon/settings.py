444c9c8f92d053efa9a0518ce1bb58c8a42c4cb6
Apoorv Deep <apoorvdeeps@gmail.com>
2025-11-08 02:06:59 +0530

    Add token authentication support to Django REST Framework settings

--- littlelemon/settings.py
+++ littlelemon/settings.py
@@ -47,6 +47,8 @@ INSTALLED_APPS = [
 
     'rest_framework',
     'djoser',
+    # Token support for DRF/Djoser
+    'rest_framework.authtoken',
 
     'restaurant',
 ]
@@ -141,3 +143,15 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Django REST Framework settings
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.AllowAny',
+    ),
+}
